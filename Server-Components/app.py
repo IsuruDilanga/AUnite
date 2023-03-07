268157896413d90408a4f83c683f8abc6355ad59
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-03-07 08:53:49 +0530

    Added memory game level 3 backend

--- Server-Components/app.py
+++ Server-Components/app.py
@@ -14,5 +14,9 @@ def mgl1():
 def mgl2():
     return render_template('MGlevel2.html')
 
+@app.route('/MGL3')
+def mgl3():
+    return render_template('MGlevel3.html')
+
 if __name__ == '__main__':
     app.run(port = 3000, debug=True)
