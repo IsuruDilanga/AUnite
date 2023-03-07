a52d576be3f64917ddb867d92342b66967cb34a9
Maduka <madukalaksahan@gmail.com>
2023-03-07 21:20:06 +0530

    Added Math & English parts in Education  to

---
+++ Client-Components/Education/English/Vegetables.js
@@ -0,0 +1,46 @@
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
+const audio3 = document.getElementById("audio3");
+const audio4 = document.getElementById("audio4");
+const audio5 = document.getElementById("audio5");
+const audio6 = document.getElementById("audio6");
+const audio7 = document.getElementById("audio7");
+const audio8 = document.getElementById("audio8");
+const audio9 = document.getElementById("audio9");
+
+
+document.getElementById("Bell pepper").addEventListener("click", () => {
+  audio1.play();
+});
+
+document.getElementById("Brinjal").addEventListener("click", () => {
+  audio2.play();
+});
+
+document.getElementById("Broccoli").addEventListener("click", () => {
+  audio3.play();
+});
+
+document.getElementById("Cabbage").addEventListener("click", () => {
+  audio4.play();
+});
+
+document.getElementById("Carrot").addEventListener("click", () => {
+  audio5.play();
+});
+
+document.getElementById("Cauliflower").addEventListener("click", () => {
+  audio6.play();
+});
+
+document.getElementById("Cucumber").addEventListener("click", () => {
+  audio7.play();
+});
+
+document.getElementById("Tomato").addEventListener("click", () => {
+  audio8.play();
+});
+
+document.getElementById("Garlic").addEventListener("click", () => {
+  audio9.play();
+});
