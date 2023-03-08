b4b76ab180c4133d77d79c8f5b262832d7decae6
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-03-08 20:09:52 +0530

    Added homepage backend

---
+++ Server-Components/static/scripts/HomePage.js
@@ -0,0 +1,24 @@
+let profileMenu = document.getElementById("profileMenu");
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+
+}
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
