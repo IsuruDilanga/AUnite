7b4a3dabef88b4fb403be80b3a2f397c77c7dbd2
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-03-08 12:32:24 +0530

    Remove AppSsignup.py file and Merge app.y file

--- Server-Components/app.py
+++ Server-Components/app.py
@@ -38,5 +38,51 @@ def login():
 
     return render_template('login.html')
 
+
+@app.route('/signup')
+def signupPage():
+    return render_template('SignUp.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    fullname = request.form['fullname']
+    email = request.form['email']
+    number = request.form['mobile']
+    dob = request.form['dob']
+    password = request.form['pass']
+    gender = ""
+
+    if(request.form['gender'] == 'male'):
+        gender = 'male'
+    elif(request.form['gender'] == 'female'):
+        gender = 'female'
+    elif(request.form['gender'] == 'NA'):
+        gender = 'Not preferred'
+
+    conn = sqlite3.connect('userData.db')
+    c1 = conn.cursor()
+
+    c1.execute('SELECT email FROM user')
+    rows1 = c1.fetchall()
+
+    emailHas = True
+    
+    emails = [row1[0] for row1 in rows1]
+
+    for eachEmail in emails:
+        if eachEmail == email:
+            emailHas = False
+            flash("The Email Address is already Registered")
+            return redirect('/signup')
+
+    if emailHas:
+        c1.execute("INSERT INTO user (email, password, fullname, number, dob, gender) VALUES (?, ?, ?, ?, ?, ?)", (email, password, fullname, number, dob, gender))
+        conn.commit()
+        return render_template('login.html')
+
+    return render_template('SignUp.html')
+    
+
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
