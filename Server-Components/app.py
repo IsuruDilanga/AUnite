8c3d5dd42fde2a8fb83003652617b4113b7d0458
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-03-13 00:12:02 +0530

    Game Section connect backend

--- Server-Components/app.py
+++ Server-Components/app.py
@@ -252,7 +252,7 @@ def predict_emotion(image):
 def meetTeam():
     return render_template('MeetTeam.html')
 
-@app.route('/entertainment')
+@app.route('/entertainments')
 def entertainment():
     return render_template('Entertainment.html')
 
@@ -266,6 +266,34 @@ def userProfile():
     dob = session.get('dob')
 
     return render_template('profile.html', fullname=fullname, email=email, number=number, dob=dob)
+
+@app.route('/game')
+def games():
+    return render_template('game.html')
+
+@app.route('/MGL1')
+def mgl1():
+    return render_template('MGlevel1.html')
+
+@app.route('/MGL2')
+def mgl2():
+    return render_template('MGlevel2.html')
+
+@app.route('/MGL3')
+def mgl3():
+    return render_template('MGlevel3.html')
+
+@app.route('/snake')
+def snake():
+    return render_template('snake.html')
+
+@app.route('/tic')
+def tic():
+    return render_template('ticTacToe.html')
+
+@app.route('/flight')
+def flight():
+    return render_template('flight.html')
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
